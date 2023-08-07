7207bba504352fb27da6d7a538497fb92fbdf5d9
ryan <ryanba08@icloud.com>
2023-08-07 17:32:19 -0400

    gettting there

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,40 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Projects extends Component {
-  render() {
-    return (
-      <div className="flex justify-center mt-20 text-2xl">
-        Under Construction!
+export default function Projects() {
+  const [effect, setEffect] = useState(true);
+  const glowingLine = "w-10/12 h-1 rounded-md mx-auto";
+  const downArrow =
+    "mx-auto w-5 h-5 border-t-[3px] border-r-[3px] border-solid botder-white rotate-[135deg] hover:border-purple-500 mt-2 ";
+  return (
+    <div className="grid text-2xl mt-40 text-center">
+      <div
+        className="hover:text-purple-500 hover:animate-bounce2"
+        onClick={() => {
+          effect ? setEffect(false) : setEffect(true);
+        }}
+      >
+        Projects
       </div>
-    );
-  }
+      <ul className="list-none mt-1">
+        <li
+          className={
+            effect
+              ? `${glowingLine} bg-slate-50 animate-pulse`
+              : `${glowingLine} bg-slate-500 animate-none translate-y-44`
+          }
+        ></li>
+      </ul>
+      <div
+        className={
+          effect
+            ? `${downArrow} animate-pulse`
+            : `${downArrow} animate-none translate-y-44`
+        }
+        onClick={() => {
+          effect ? setEffect(false) : setEffect(true);
+        }}
+      ></div>
+      <div className={effect ? "hidden" : ""}>Under Construction!</div>
+    </div>
+  );
 }
-
-export default Projects;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import IceCream from "./components/IceCream";
-import Nav from "./components/Nav";
 import Projects from "./components/Projects";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div>
-    <Nav />
+  <html>
     <Projects />
-  </div>
+  </html>
 );
