d58640d1cf7897203869ad7a1005e895a16b1846
ryan <ryanba08@icloud.com>
2023-08-09 17:18:58 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import ReactDOM from "react-dom";
 import Projects from "./pages/Projects";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./app.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
@@ -9,7 +10,7 @@ root.render(<App />);
 function App() {
   return (
     <div className="App">
-      <Router basename="/">
+      <Router basename="/ryanba08.github.io">
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
