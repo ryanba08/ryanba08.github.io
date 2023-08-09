75bbf4fc31a344064a9efa7a061b7c3c04cc8c30
ryan <ryanba08@icloud.com>
2023-08-09 16:15:05 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/ryanba08.github.io" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
       </Router>
