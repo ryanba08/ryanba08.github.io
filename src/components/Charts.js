4b5757c3f93aa21ab7a56c5ba486b752f73da266
ryan <ryanba08@icloud.com>
2023-08-08 21:25:58 -0400

    changes

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -3,10 +3,12 @@ import React from "react";
 function Charts() {
   {
     return (
-      <div>
+      <div className="flex justify-center mt-20">
         <img
-          src="https://raw.githubusercontent.com/ryanba08/ryanba08.github.io/main/src/assets/analystCharts.png"
+          src="https://raw.githubusercontent.com/ryanba08/ryanba08.github.io/main/src/assets/analystChart.png"
           alt="fail"
+          width="1200"
+          height="600"
         />
       </div>
     );
