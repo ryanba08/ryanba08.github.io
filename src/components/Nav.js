7e8fd18f8e8b456d123fb53861b2b7d89012fbab
ryan <ryanba08@icloud.com>
2023-08-11 11:57:07 -0400

    changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
   const textStyle = "mr-4 md:mr-8 hover:text-purple-500 hover:animate-bounce2";
   const navigate = useNavigate();
+  const [effect, setEffect] = useState(false);
   return (
     <div className="z10 flex justify-between w-full mt-3 md:w-11/12 text-slate-50">
       <div></div>
@@ -11,12 +12,39 @@ export default function Nav() {
         <li className={textStyle}>
           <button onClick={() => navigate("/projects")}>Projects</button>
         </li>
-        <li className={textStyle}>
-          <a href="https://github.com/ryanba08" target="_blank">
-            Github
-          </a>
+        <li
+          className={textStyle}
+          onClick={() => (effect ? setEffect(false) : setEffect(true))}
+        >
+          Contact
         </li>
-        <li className={textStyle}>Contact</li>
+        <li
+          className={
+            effect
+              ? "z-10 w-60 h-40 bg-slate-400 mt-10 -ml-28 md:-ml-8 absolute rounded flex align-middle flex-col text-black"
+              : "hidden"
+          }
+        >
+          <span className="mx-auto mt-4 ml-4 leading-10">
+            <a
+              className="hover:text-purple-500"
+              href="https://github.com/ryanba08"
+              target="_blank"
+            >
+              Github
+            </a>
+            <br />
+            <a
+              className="hover:text-purple-500"
+              href="https://www.linkedin.com/in/ryan-cannady-387518226/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+            <br />
+            ryanba08@icloud.com
+          </span>
+        </li>
       </ul>
     </div>
   );
